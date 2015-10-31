99f9cbf4bc8e3b3d42946a26c002a6a3ebd270d0
ChoYuJin0525 <dbwls5025@naver.com>
2015-10-31 11:11:47 +0900

    1013
    엑셀, 파이썬

---
+++ 1013.py
@@ -0,0 +1,10 @@
+for i in range(10):
+    if i == 5: break
+    print i
+print "EOP"
+
+for i in range(10):
+    if i==5: continue
+    print i
+print "EOP"
+
---
+++ a.py
@@ -0,0 +1,9 @@
+i=1
+while i<10:
+        print i
+        i+=1
+
+
+for i in range(0,5):
+    print i
+
